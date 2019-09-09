81ab11bac59ef9744472f50b9c682fcaf6f63b59
zhu-hao-liang <1683331891@qq.com>
2019-09-09 14:53:32 +0800

    init

---
+++ src/views/shopList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    shopList
+  </div>
+</template>
+
+<script>
+export default {
+  name:"",
+  components: {},
+  props: {},
+  computed: {},
+  data() {
+    return {
+
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+  activited() {},
+  beforeRouteUpdate() {},
+  filter: {},
+  watch: {},
+};
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
